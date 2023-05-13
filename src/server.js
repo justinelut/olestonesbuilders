2072a845cd2b3aa5ce998a58a0efcebec38651a6
David <muriithid05@gmail.com>
2023-05-13 08:50:48 +0000

    Update

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ const start = async () => {
 
   // Add your own express routes here
   app.use(express.json())
-  app.use("/", express.static(path.resolve(__dirname, './frontend')))
+  app.use("/", express.static(path.resolve(__dirname, '/public')))
 
   app.listen(process.env.PORT);
 }
