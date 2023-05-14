16178940b6c17fa22f6720c95bb50857f4387074
David <muriithid05@gmail.com>
2023-05-14 16:11:20 +0000

    Updated icons and logo

--- src/components/favicon.js
+++ src/components/favicon.js
@@ -4,6 +4,6 @@ import React from 'react'
 export default function Icon() {
 
     return (
-        <img style={{ width: '70px', height: '70px' }} src="/assets/favicon.png" alt="favicon" />
+        <img src="/assets/favicon.png" alt="favicon" />
     )
 }
